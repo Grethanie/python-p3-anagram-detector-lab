aaa4705bcdfa1f2a44c106cdfc077c755d934a6f
Daniel Young <Danyoung91@gmail.com>
2023-09-26 23:45:15 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+
+
+class Anagram:
+    
+    def __init__(self, word):        
+        self.word = word
+    
+    def match(self, list):       
+       
+
+                
+            
+            
+        return [item for item in list if sorted([letter for letter in self.word]) == sorted([letter for letter in item])]
+        
+        
+    
+   
